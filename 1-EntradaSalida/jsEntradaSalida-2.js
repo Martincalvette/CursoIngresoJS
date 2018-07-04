7093b071c4dbebafb84f179bb35de813fdc9677b
Martincalvette <40797865+Martincalvette@users.noreply.github.com>
2018-07-03 21:51:08 -0300

    Primer guardado
    Son los ejercicios del 1 al 4
    
    
    (importancia del orden de = y value. aparte de no usar comillas cuando se trata de var)

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,18 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//var person = prompt("Please enter your name", "Harry Potter");
+	//comentario de una linea
+	/* 
+		Esto es un comentario en bloque
+		sirve para tomar apunte
+	*/
+	var nombre; 
+	//Cuidar la semantica//
+	//alert(nombre);
+	//nombre = "Martin Calvette";
+	nombre = prompt ("ingrese nombre","natalia natalia");
+	alert("Su nombre es" + nombre);
 }
+
 

c88e1df650d3c552b6e379b84404c2513e8b9a31
Martincalvette <40797865+Martincalvette@users.noreply.github.com>
2018-07-06 00:15:17 -0300

    entrada 2
    Logre hacer el punto 5 luego de haberlo perdido en clase.
    
    Quede trabado en el punto 6 ante una serie de errores que no termine de solucionar

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,21 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var valorA;
+	var valorB;
+	var resultado;
+
+		valorA = document.getElementById('numeroUno').value;
+		valorB = document.getElementById('numeroDos').value;
+
+			var resultado = parseInt(numeroUno) + (numeroDos);
+
+			alert ("la suma es" resultado );
+
+			//jsEntradaSalida-6.js:15 Uncaught SyntaxError: missing ) after argument list
+
+			
+}
+

2b2bf11af86becc6415aadb2319dea2cd81fdba0
Martincalvette <40797865+Martincalvette@users.noreply.github.com>
2018-07-06 22:22:19 -0300

    -
    --

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,12 +1,40 @@
+/* Se ingresan tres numeros,se muestra la suma de los tres, el promedio de los tres 
+y la resta de los tres en un unico ALERT */
+
+
+
 
 function mostrar()
 {
-	var base;
+	
+	var numero1;
+	var numero2;
+	var numero3;
+	var suma;
+	var promedio;
+	var resta;
+
+	prompt(" Ingrese numero 1 ")=numero1;
+
+	prompt(" Ingrese numero 1 ")=numero2;
+
+	prompt(" Ingrese numero 1 ")=numero3;
+
+	suma = parseInt(numero1+numero2+numero3);
+
+	promedio = parseInt(numero1*numero2*numero3 / 3);
+
+	resta = parseInt(numero1-numero2-numero3);
+ 
+
+	alert( " la suma es" + suma " el promedio es " + promedio " la resta es " + resta)
+
+	/*var base;
 	var	altura;	
 	var perimetro;
 	var superficie;
 
-
+/*
 		base=prompt(" base ");
 		altura=prompt(" altura ");
 
@@ -17,7 +45,7 @@ function mostrar()
 		//(base * 2)=superficie
 		//(altura * 3)=perimetro
 
-		alert(" El perimetro es igual a " + perimetro "la superficie es igual a" + superficie);
+		alert(" El perimetro es igual a " + perimetro "la superficie es igual a" + superficie);*/
 
 
 }

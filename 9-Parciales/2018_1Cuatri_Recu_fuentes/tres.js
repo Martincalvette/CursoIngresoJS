d6a7cd88bfe30cc39ccfae17598f855ee341134a
Martincalvette <40797865+Martincalvette@users.noreply.github.com>
2018-07-06 21:55:57 -0300

    prueba parcial-
    lagunas constantes.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+	/*var precio;
+	var porcentajeDeDescuento;
+	var precioConDescuento;
+	var IVA;
+
+	precio= prompt("el precio es");
+	porcentajeDeDescuento=prompt("el % de descuento");
+
+	precioConDescuento= (precio - porcentajeDeDescuento);
+
+	alert (precioConDescuento);
+/*
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,23 @@
 
 function mostrar()
 {
+	var base;
+	var	altura;	
+	var perimetro;
+	var superficie;
+
+
+		base=prompt(" base ");
+		altura=prompt(" altura ");
+
+		superficie=parseInt(base) * 2;
+
+		perimetro=parseInt(altura) * ;
+
+		//(base * 2)=superficie
+		//(altura * 3)=perimetro
+
+		alert(" El perimetro es igual a " + perimetro "la superficie es igual a" + superficie);
+
 
 }

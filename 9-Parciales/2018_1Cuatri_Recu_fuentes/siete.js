1124a23b66d3ee8044f01aa5d0888a50a7644561
Martincalvette <40797865+Martincalvette@users.noreply.github.com>
2018-08-02 09:09:24 -0300

    Proceso ej 7
    Interrupcion

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+	var nota;
+	var edad;
+	var sexo;
+	var contador;
+	var acumulador;
+	var promedio;
+	var varonesMayoresNotaMayor6;
+	
+
+
+	contador=0;
+	acumulador=0;
+
+	while(contador<5)
+	{
+		nota=prompt("Cual es la nota?")
+			while(nota<10 && nota>0)
+			{
+				nota=prompt("Ingrese un numero entre 0 y 10")
+			}
+		edad=prompt("Ingrese la edad")
+
+		sexo=prompt("Ingrese el sexo")
+			while (sexo!="F" || sexo!="m")
+			{
+				sexo=prompt("Ingrese un sexo valido")
+			}
+	}
 
 }

21f739c1a24af03559d5f053d55e291098bf28de
Martincalvette <40797865+Martincalvette@users.noreply.github.com>
2018-08-03 16:26:13 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -7,6 +7,7 @@ function mostrar()
 	var acumulador;
 	var promedio;
 	var varonesMayoresNotaMayor6;
+	var notasTotales;
 	
 
 
@@ -16,17 +17,28 @@ function mostrar()
 	while(contador<5)
 	{
 		nota=prompt("Cual es la nota?")
-			while(nota<10 && nota>0)
+			while( nota!=0 && nota>10)
 			{
 				nota=prompt("Ingrese un numero entre 0 y 10")
 			}
+
+			acumulador=acumulador+nota;
+
 		edad=prompt("Ingrese la edad")
 
 		sexo=prompt("Ingrese el sexo")
-			while (sexo!="F" || sexo!="m")
+			while (sexo!="F" && sexo!="f" && sexo!="M" && sexo!="m")
 			{
 				sexo=prompt("Ingrese un sexo valido")
 			}
+			
+		contador++;
 	}
 
+	promedio= acumulador / contador;
+
+	alert (promedio)
+
+
 }
+ //NO FUNCIONA
